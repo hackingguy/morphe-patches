86886d0f0e42fa31b57e15862f30289579163f33
Akash Chhabra <akash@hackingguy.local>
2026-04-05 02:27:21 +0530

    feat: Added patches for at4k and bounce

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,13 +1,13 @@
-group = "app.morphe"
+group = "app.bounce"
 
 patches {
     about {
-        name = "XYZ Patches for use with Morphe"
-        description = "Example patches"
-        source = "git@github.com:MorpheApp/morphe-patches-template.git"
-        author = "Awesome dev"
-        contact = "na"
-        website = "https://morphe.software"
+        name = "hackingguy patches"
+        description = "hackingguy patches"
+        author = "hackingguy"
+        website = "https://github.com/hackingguy/morphe-patches"
+        source = "git@github.com:hackingguy/morphe-patches"
+        contact = ""
         license = "GPLv3"
     }
 }
